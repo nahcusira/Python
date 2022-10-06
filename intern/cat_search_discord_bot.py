298a207b88b3bd93e5101e1b443ec38c31c5ba97
Kami-sama <87233531+arisu-sakayanagi@users.noreply.github.com>
2022-10-06 15:25:22 +0700

    Update cat_search_discord_bot.py

--- intern/cat_search_discord_bot.py
+++ intern/cat_search_discord_bot.py
@@ -6,9 +6,10 @@ import requests
 
 bot = commands.Bot(command_prefix="!", intents=discord.Intents.all())
 
-@bot.command()
+
+@bot.command('random')
 async def cat(ctx):
-    embed = discord.Embed() 
+    embed = discord.Embed()
     open('tmp.png', 'wb').write(requests.get('https://cataas.com/cat').content)
     file = discord.File("tmp.png", filename="image.png")
     embed.set_image(url="attachment://image.png")
@@ -17,7 +18,16 @@ async def cat(ctx):
 
 @bot.event
 async def on_message(message):
-    if message.content == 
+    if str(message.content).startswith('cat'):
+        channel = bot.get_channel('1025230130806784013')
+        embed = discord.Embed()
+        open('tmp.png', 'wb').write(requests.get(
+            'https://cataas.com/cat/says/'+message.content[4:]).content)
+        file = discord.File("tmp.png", filename="image.png")
+        embed.set_image(url="attachment://image.png")
+        channel = await bot.fetch_channel('1025230130806784013')
+        await channel.send(file=file, embed=embed)
+
 
 @tasks.loop(minutes=5)
 async def send_message():
@@ -28,9 +38,11 @@ async def send_message():
     embed.set_image(url="attachment://image.png")
     channel = await bot.fetch_channel('1025230130806784013')
     await channel.send(file=file, embed=embed)
+
+
 @bot.event
 async def on_ready():
     send_message.start()
 
 
-bot.run("MTAyNTI0NTc4MTk4MjE5NTc1Mw.GLBt73.3C26CBocjD4NsF-phvhckD0SPHjknVKBxGA8jo")
+bot.run("MTAyNzIxNjEyNDQ1MjAxNjE4OA.Gyciuh.8RA09mygfrYwTYc4mDa7oLzKllrpWq6u-3lp7w")
