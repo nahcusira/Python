5d64cbbada49316e3a887e2176ed94aae786f1aa
Kami-sama <87233531+arisu-sakayanagi@users.noreply.github.com>
2022-10-10 14:52:06 +0700

    Update file_format_convert.py

--- intern/file_format_convert.py
+++ intern/file_format_convert.py
@@ -1,13 +1,15 @@
-import json
-import csv
+import json, csv, argparse
 
 
+parser = argparse.ArgumentParser()
+parser.add_argument('--input','--input', type=str, help='input file name')
+parser.add_argument('--output','--output', type=str, help='output file name')
+args = parser.parse_args()
 
-
-with open('data.json') as json_file:
+with open(args.input) as json_file:
     data = json.load(json_file)
 employee_data = data['emp_details']
-data_file = open('data_file.csv', 'w')
+data_file = open(args.output, 'w')
 csv_writer = csv.writer(data_file)
 count = 0
 for emp in employee_data:
@@ -17,3 +19,35 @@ for emp in employee_data:
         count += 1
     csv_writer.writerow(emp.values())
 data_file.close()
+print('Successfully created')
+
+
+
+
+
+
+
+# import json
+# import csv
+
+
+
+
+# with open('data.json') as json_file:
+#     data = json.load(json_file)
+# employee_data = data['emp_details']
+# data_file = open('data_file.csv', 'w')
+# csv_writer = csv.writer(data_file)
+# count = 0
+# for emp in employee_data:
+#     if count == 0:
+#         header = emp.keys()
+#         csv_writer.writerow(header)
+#         count += 1
+#     csv_writer.writerow(emp.values())
+# data_file.close()
+
+
+
+
+
