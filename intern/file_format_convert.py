f1dbaa9c59126bd641af0baaf8beb2829ce47d4b
Kami-sama <87233531+arisu-sakayanagi@users.noreply.github.com>
2022-10-10 15:29:06 +0700

    Update file_format_convert.py

--- intern/file_format_convert.py
+++ intern/file_format_convert.py
@@ -1,25 +1,85 @@
 import json, csv, argparse
-
+import pandas as pd
+from openpyxl import Workbook
 
 parser = argparse.ArgumentParser()
 parser.add_argument('--input','--input', type=str, help='input file name')
 parser.add_argument('--output','--output', type=str, help='output file name')
-args = parser.parse_args()
+parser.add_argument('-jsontocsv', '--result', action='store_true', help='json to csv')
+parser.add_argument('-jsontoxlsx', '--result1', action='store_true', help='json to xlsx')
 
-with open(args.input) as json_file:
-    data = json.load(json_file)
-employee_data = data['emp_details']
-data_file = open(args.output, 'w')
-csv_writer = csv.writer(data_file)
-count = 0
-for emp in employee_data:
-    if count == 0:
-        header = emp.keys()
-        csv_writer.writerow(header)
-        count += 1
-    csv_writer.writerow(emp.values())
-data_file.close()
-print('Successfully created')
+args = parser.parse_args()
+if args.result:
+    with open(args.input) as json_file:
+        data = json.load(json_file)
+    employee_data = data['emp_details']
+    data_file = open(args.output, 'w')
+    csv_writer = csv.writer(data_file)
+    count = 0
+    for emp in employee_data:
+        if count == 0:
+            header = emp.keys()
+            csv_writer.writerow(header)
+            count += 1
+        csv_writer.writerow(emp.values())
+    data_file.close()
+    print('Successfully json to csv file')
+if args.result1:
+    with open(args.input) as json_file:
+        data = json.load(json_file)
+    employee_data = data['emp_details']
+    data_file = open('data.csv', 'w')
+    csv_writer = csv.writer(data_file)
+    count = 0
+    for emp in employee_data:
+        if count == 0:
+            header = emp.keys()
+            csv_writer.writerow(header)
+            count += 1
+        csv_writer.writerow(emp.values())
+    data_file.close()
+    wb = Workbook()
+    ws = wb.active
+    with open('data.csv', 'r') as f:
+        for row in csv.reader(f):
+            ws.append(row)
+    wb.save('data.xlsx')
+    print('Successfully json to xlsx file')
+
+
+
+
+
+
+
+
+
+
+
+
+
+# import json, csv, argparse
+
+
+# parser = argparse.ArgumentParser()
+# parser.add_argument('--input','--input', type=str, help='input file name')
+# parser.add_argument('--output','--output', type=str, help='output file name')
+# args = parser.parse_args()
+
+# with open(args.input) as json_file:
+#     data = json.load(json_file)
+# employee_data = data['emp_details']
+# data_file = open(args.output, 'w')
+# csv_writer = csv.writer(data_file)
+# count = 0
+# for emp in employee_data:
+#     if count == 0:
+#         header = emp.keys()
+#         csv_writer.writerow(header)
+#         count += 1
+#     csv_writer.writerow(emp.values())
+# data_file.close()
+# print('Successfully created')
 
 
 
